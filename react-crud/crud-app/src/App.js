1277bff0f0a1aca04b7a56fab8b90452a99c5dc1
rcalixto <robsoncaliixto@gmail.com>
2018-06-12 22:40:15 -0300

    criação do primeiro componente

--- react-crud/crud-app/src/App.js
+++ react-crud/crud-app/src/App.js
@@ -1,55 +1,93 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import InputCustomizado from './componentes/InputCustomizado';
+import SubmitCustomizado from './componentes/SubmitCustomizado';
 
 
 class App extends Component {
+
   constructor(){
     super();
-    this.state = { lista: [{nome: 'Robson',email:'robson@calixto.com',senha:'123456789'}] };
+    this.state     = { lista: [], nome:'', email:'', senha:'' };
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome   = this.setNome.bind(this);
+    this.setEmail  = this.setEmail.bind(this);
+    this.setSenha  = this.setSenha.bind(this);
   }
+    
+  componentWillMount(){
+    
+    fetch('http://localhost:8080/api/autores',{
+        method: 'GET',
+        headers: new Headers(),
+        mode: 'cors',
+        cache: 'default'
+      }).then( data => data.json())
+        .then( datajson => this.setState({lista:datajson}))
+  }
+
+  enviaForm(evento){
+    evento.preventDefault();
+    
+ 
+    fetch('http://localhost:8080/api/autores',{
+      method:'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ nome: this.state.nome , email: this.state.email, senha: this.state.senha })
+    })
+    .then( data => data.json())
+    .then( response => this.setState({lista:response}))
+    .catch(Error => console.error(Error));
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value});
+  }
+
+  setEmail(evento){
+    this.setState({email:evento.target.value});
+  }
+
+  setSenha(evento){
+    this.setState({senha:evento.target.value});
+  }
+  
   render() {
     return (     
             <main id="layout">
-              <a href="#menu" id="menuLink" class="menu-link"><span></span></a>
+              <a href="#menu" id="menuLink" className="menu-link"><span></span></a>
               <nav id="menu">
-                  <div class="pure-menu">
-                      <a class="pure-menu-heading" href="#">Crud React</a>
-                      <ul class="pure-menu-list">
-                          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
-                          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Autor</a></li>
-                          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Livro</a></li>
+                  <div className="pure-menu">
+                      <a className="pure-menu-heading" >Crud React</a>
+                      <ul className="pure-menu-list">
+                          <li className="pure-menu-item"><a className="pure-menu-link">Home</a></li>
+                          <li className="pure-menu-item"><a  className="pure-menu-link">Autor</a></li>
+                          <li className="pure-menu-item"><a  className="pure-menu-link">Livro</a></li>
                       </ul>
                   </div>
               </nav>
               <header id="main">
-                  <div class="header">
+                  <div className="header">
                       <h1>Autor</h1>
                   </div>
               </header>
-              <article class="content" id="content">
-                <section class="pure-form pure-form-aligned">
-                    <form class="pure-form pure-form-aligned">
-                      <div class="pure-control-group">
-                        <label htmlFor="nome">Nome</label> 
-                        <input id="nome" type="text" name="nome" value=""  />                  
-                      </div>
-                      <div class="pure-control-group">
-                        <label htmlFor="email">Email</label> 
-                        <input id="email" type="email" name="email" value=""  />                  
-                      </div>
-                      <div class="pure-control-group">
-                        <label htmlFor="senha">Senha</label> 
-                        <input id="senha" type="password" name="senha"  />                                      
-                      </div>
-                      <div class="pure-control-group">                                  
-                        <label></label> 
-                        <button type="submit" class="pure-button pure-button-primary">Gravar</button>                                    
-                      </div>
+              <article className="content" id="content">
+                <section className="pure-form pure-form-aligned">
+                    <form className="pure-form pure-form-aligned" onSubmit= { this.enviaForm } method="POST">
+                      
+                      <InputCustomizado id="nome"  type="text"     name="nome"  value={this.state.nome}  onChange={this.setNome}  label="Nome"  />                                              
+                      <InputCustomizado id="email" type="email"    name="email" value={this.state.email} onChange={this.setEmail} label="Email" />                                              
+                      <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />  
+                      <SubmitCustomizado type="submit" label="Gravar" />
+
                     </form>             
                   </section>  
                   <section>            
-                      <table class="pure-table">
+                      <table className="pure-table">
                         <thead>
                           <tr>
                             <th>Nome</th>
@@ -60,7 +98,7 @@ class App extends Component {
                           {
                             this.state.lista.map( autor => {
                               return (
-                                <tr>
+                                <tr key= { autor.id }>
                                   <td>{autor.nome}</td>
                                   <td>{autor.email}</td>
                                 </tr>
